297053c13ab2c0d82715353fc573209eeb50f60b
✩°｡⋆⸜ TaXi0k ✮ ᯓ★ <igorciu@interia.pl>
2026-01-13 22:30:34 +0100

    Minor update
    Added RESET button and some animations

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const gridItems = document.querySelectorAll('.gridItem');
+const resetButton = document.querySelector('.resetButton');
+
+resetButton.addEventListener('click', function () {
+    resetButton.addEventListener('click', function () {
+        gridItems.forEach(item => {
+            item.classList.remove('marked');
+        });
+    });
+});
 
 gridItems.forEach(item => {
     item.addEventListener('click', function() {
